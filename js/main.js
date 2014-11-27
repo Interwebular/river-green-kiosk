92650a319917107d83b54f408a5569a950e24352
br4nt <brant.wladichuk@azubu.com>
2014-11-26 16:28:20 -0800

    added ajax

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 
+(function( $ ){
+	$('.load-page').click(function(e){
+		var page = 'ajax/' + $(this).data('page') + '.html';
+		$('#loaded-page-content').load(page + " #page-container");
+		e.preventDefault();
+	});
+})( jQuery );
