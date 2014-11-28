95191ae8b95b8695a1ccabf0b1a71734ff6f025a
Lorne Markham <lorne.markham@azubu.com>
2014-11-28 13:34:17 -0800

    left and right navigation :

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 
 
 (function( $ ){
-$('.load-page').click(function(e){
+$('#left-nav-wrap .load-page').click(function(e){
 	var whatPage = $(this).data('page');
 	var page = 'ajax/' + $(this).data('page') + '.html';
 	var topMargin = $(this).data('height');
 
 
-	$('#loaded-page-content').load(page + " #page-container");
-	$('#loaded-page-content').parent().css({ top : topMargin + 'px' });
+	$('#loaded-page-content-left').load(page + " #page-container");
+	$('#loaded-page-content-left').parent().css({ top : topMargin + 'px' });
 	
 
-	if(! $('.page-wrap').hasClass('open')){
-		$( ".page-wrap" ).addClass( 'open' );
+	if(! $('.page-wrap-left').hasClass('open')){
+		$( ".page-wrap-left" ).addClass( 'open' );
 	}
 	
 	if( whatPage == 'quick-facts'){
@@ -27,16 +27,57 @@ $('.load-page').click(function(e){
 });
 
 
-$( ".close-pages" ).on( "click", function() {
-  $( ".page-wrap" ).removeClass( 'open' );
+$('#right-nav-wrap .load-page').click(function(e){
+	var whatPage = $(this).data('page');
+	var page = 'ajax/' + $(this).data('page') + '.html';
+	var topMargin = $(this).data('height');
+
+
+	$('#loaded-page-content-right').load(page + " #page-container");
+	$('#loaded-page-content-right').parent().css({ top : topMargin + 'px' });
+	
+
+	if(! $('.page-wrap-right').hasClass('open')){
+		$( ".page-wrap-right" ).addClass( 'open' );
+	}
+	
+	if( whatPage == 'quick-facts'){
+		$('.page-wrap-right .arrows').css({ 'margin-top' : '380px' });
+	}
+
+	else{
+		$('.page-wrap-right .arrows').css({ 'margin-top' : '280px' });
+	}
+
+	e.preventDefault();
 });
 
 
 
-$( "#menu-toggle" ).on( "click", function() {
-  $( "nav" ).toggleClass( 'open' );
+
+
+
+$( ".close-pages.left" ).on( "click", function() {
+  $( ".page-wrap-left" ).removeClass( 'open' );
 });
 
+$( ".close-pages.right" ).on( "click", function() {
+  $( ".page-wrap-right" ).removeClass( 'open' );
+});
+
+
+$( "#menu-toggle-left" ).on( "click", function() {
+  $( ".nav-left" ).toggleClass( 'open' );
+});
+
+$( "#menu-toggle-right" ).on( "click", function() {
+  $( ".nav-right" ).toggleClass( 'open' );
+});
+
+$( ".hotspot-dot" ).on( "click", function() {
+  $( this ).toggleClass( 'on' );	
+  $( ".hotspot-popup" ).toggleClass( 'on' );
+});
 
 
 
