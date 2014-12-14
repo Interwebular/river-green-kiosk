675c668b83ad53b6176920dcbd18a59311e56a2c
Lorne Markham <lorne.markham@azubu.com>
2014-12-13 17:26:51 -0800

    merge with brant

--- index.php
+++ index.php
@@ -5,158 +5,251 @@
 <!--[if gt IE 8]><!--> 
 
 <html class="no-js"> <!--<![endif]-->
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>River Green Kiosk</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/main.css">
-			<link rel="stylesheet" href="css/style.css">
-    <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>River Green Kiosk</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+				<link rel="stylesheet" href="css/style.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
 
-</head>
-<body>
-	
-	<div id="viewport-container">
+    </head>
+    <body>
+		
+		
+		
+		<div id="viewport-container">
 
-    <div id="left-nav-wrap">
-        <div id="logo-wrap"></div>
-        <nav class="nav-left">
-            <ul>
-                <li class="load-page" data-page="the-views" data-height="90"><span>check out</span>the views</li>
-                <li class="load-page" data-page="amenities" data-height="200"><span>view the</span>amenities</li>
-                <li class="load-page" data-page="the-suites" data-height="310"><span>see inside</span>the suites</li>
-                <li class="load-page" data-page="community" data-height="420"><span>learn about</span>community</li>
-                <li class="load-page" data-page="quick-facts" data-height="435"><span>view our</span>quick facts</li>
-            </ul>
-        </nav>
-        <div id="menu-toggle-left" class="menu-toggle"></div>
-    </div>
-	
-    <div id="page-views-left" class="page-wrap-left">
-        
-		<span class="left-arrow arrows"></span>
-        <span class="close-pages left">X</span>
-        <div id="loaded-page-content-left">
-			<?php require(__DIR__.'/includes/page-sections-index.php'); ?>
-		</div>
-        
-    </div>
-	
-     <div id="page-views-right" class="page-wrap-right">
-        <span class="right-arrow arrows"></span>
-        <span class="close-pages right">X</span>
-        <div id="loaded-page-content-right"></div>
-    </div>
-
-    <div id="zoom-out" class="out">
-        <img src="img/map-zoom-out.jpg" id="map-img-zoomed-out" />
-    </div>
-
-	<div id="hotspots-in">
-		<div class="hotspot-wrap">
-			<h1>IN</h1>
-		</div>
-	</div>
-	<div id="hotspots-out">
-		<div class="hotspot-wrap">
-			<h1>OUT</h1>
-		</div>
-	</div> 
-	
-    <div id="zo-hotspot-1" class="hotspot-dot">
-    	<span class="border"></span>
-    	<div class="hotspot-wrap">
-       		<div id="hotspot-popup-1" class="hotspot-popup"></div>
-    	</div>
-    </div>
-								
-	<div id="zoom-parent">
-		 <img class="animation-image" id="default_image" src="img/zoom_animation/multi_touch_zoom_00000.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00001.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00002.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00003.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00004.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00005.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00006.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00007.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00008.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00009.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00010.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00011.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00012.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00013.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00014.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00015.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00016.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00017.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00018.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00019.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00020.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00021.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00022.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00023.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00024.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00025.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00026.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00027.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00028.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00029.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00030.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00031.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00032.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00033.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00034.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00035.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00036.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00037.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00038.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00039.jpg">
-		 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00040.jpg">
-	</div> 
-
-
-     <div id="right-nav-wrap">
-        <div id="logo-wrap"></div>
-        <nav class="nav-right">
-            <ul>
-                <li class="load-page" data-page="the-views" data-height="90"><span>check out</span>the views</li>
-                <li class="load-page" data-page="amenities" data-height="200"><span>view the</span>amenities</li>
-                <li class="load-page" data-page="the-suites" data-height="310"><span>see inside</span>the suites</li>
-                <li class="load-page" data-page="community" data-height="420"><span>learn about</span>community</li>
-                <li class="load-page" data-page="quick-facts" data-height="525"><span>view our</span>quick facts</li>
-            </ul>
-        </nav>
-        <div id="menu-toggle-right" class="menu-toggle">asdfadsf</div>
-    </div>
-    
-    <div id="zoom-controls-wrapper">
-        <div id="zoom-controls">
-            <div id="zoom_out">-</div>
-            ZOOM
-            <div id="zoom_in">+</div>
+        <div id="left-nav-wrap">
+            <div id="logo-wrap"></div>
+            <nav class="nav-left">
+                <ul>
+                    <li class="load-page" data-page="the-views" data-height="90"><span>check out</span>the views</li>
+                    <li class="load-page" data-page="amenities" data-height="200"><span>view the</span>amenities</li>
+                    <li class="load-page" data-page="the-suites" data-height="310"><span>see inside</span>the suites</li>
+                    <li class="load-page" data-page="community" data-height="420"><span>learn about</span>community</li>
+                    <li class="load-page" data-page="quick-facts" data-height="435"><span>view our</span>quick facts</li>
+                </ul>
+            </nav>
+            <div id="menu-toggle-left" class="menu-toggle"></div>
         </div>
-    </div>
+		
+        <div id="page-views-left" class="page-wrap-left">
+            
+			<span class="left-arrow arrows"></span>
+            <span class="close-pages left">X</span>
+            <div id="loaded-page-content-left">
+				<?php require(__DIR__.'/includes/page-sections-index.php'); ?>
+			</div>
+            
+        </div>
 
-	</div> <!-- /viewport-container  -->
+				
+				
+         <div id="page-views-right" class="page-wrap-right">
+            <span class="right-arrow arrows"></span>
+            <span class="close-pages right">X</span>
+            <div id="loaded-page-content-right"></div>
+        </div>
+<!-- 
+        <div id="zoom-out" class="out">
+            <img src="img/map-zoom-out.jpg" id="map-img-zoomed-out" />
+        </div>
+ -->
+		<div id="hotspots-in">
+			<div class="hotspot-wrap">
 
-    
-
-    <script src="js/vendor/jquery-2.1.1.min.js"></script>
-    <script src="js/vendor/jquery.panorama.js"></script>
-			<script src="js/animation.js"></script>
-    <script src="js/main.js"></script>
+			<div id="zi-hotspot-1" class="hotspot-dot">
+	        	<span class="border"></span>
+	        	<div class="hotspot-wrap">
+	           		<div id="hotspot-popup-1" class="hotspot-popup"></div>
+	        	</div>
+	        </div>
 			
-    <script>
-        $(function(){
-            $("img.advancedpanorama").panorama({
-                 auto_start: 0,
-                start_position: 1527
-                });
 
-        });
-    </script>
-</body>
+			<div id="zi-hotspot-2" class="hotspot-dot">
+	        	<span class="border"></span>
+	        	<div class="hotspot-wrap">
+	           		<div id="hotspot-popup-1" class="hotspot-popup"></div>
+	        	</div>
+	        </div>
+
+	        <div id="zi-hotspot-3" class="hotspot-dot">
+	        	<span class="border"></span>
+	        	<div class="hotspot-wrap">
+	           		<div id="hotspot-popup-1" class="hotspot-popup"></div>
+	        	</div>
+	        </div>
+
+	        <div id="zi-hotspot-4" class="hotspot-dot">
+	        	<span class="border"></span>
+	        	<div class="hotspot-wrap">
+	           		<div id="hotspot-popup-1" class="hotspot-popup"></div>
+	        	</div>
+	        </div>
+
+	        <div id="zi-hotspot-5" class="hotspot-dot">
+	        	<span class="border"></span>
+	        	<div class="hotspot-wrap">
+	           		<div id="hotspot-popup-1" class="hotspot-popup"></div>
+	        	</div>
+	        </div>
+
+	        <div id="zi-hotspot-6" class="hotspot-dot">
+	        	<span class="border"></span>
+	        	<div class="hotspot-wrap">
+	           		<div id="hotspot-popup-1" class="hotspot-popup"></div>
+	        	</div>
+	        </div>
+			</div> <!-- hotspot wrap -->
+		</div> <!-- hotspot in -->
+
+
+
+
+
+		<div id="hotspots-out">
+			<div class="hotspot-wrap">
+				
+			<div id="zo-hotspot-1" class="hotspot-dot">
+	        	<span class="border"></span>
+	        	<div class="hotspot-wrap">
+	           		<div id="hotspot-popup-1" class="hotspot-popup"></div>
+	        	</div>
+	        </div>
+			
+
+			<div id="zo-hotspot-2" class="hotspot-dot">
+	        	<span class="border"></span>
+	        	<div class="hotspot-wrap">
+	           		<div id="hotspot-popup-1" class="hotspot-popup"></div>
+	        	</div>
+	        </div>
+
+	        <div id="zo-hotspot-3" class="hotspot-dot">
+	        	<span class="border"></span>
+	        	<div class="hotspot-wrap">
+	           		<div id="hotspot-popup-1" class="hotspot-popup"></div>
+	        	</div>
+	        </div>
+
+	        <div id="zo-hotspot-4" class="hotspot-dot">
+	        	<span class="border"></span>
+	        	<div class="hotspot-wrap">
+	           		<div id="hotspot-popup-1" class="hotspot-popup"></div>
+	        	</div>
+	        </div>
+
+	        <div id="zo-hotspot-5" class="hotspot-dot">
+	        	<span class="border"></span>
+	        	<div class="hotspot-wrap">
+	           		<div id="hotspot-popup-1" class="hotspot-popup"></div>
+	        	</div>
+	        </div>
+
+	        <div id="zo-hotspot-6" class="hotspot-dot">
+	        	<span class="border"></span>
+	        	<div class="hotspot-wrap">
+	           		<div id="hotspot-popup-1" class="hotspot-popup"></div>
+	        	</div>
+	        </div>
+
+
+			</div> <!-- hotspot wrap -->
+		</div>  <!-- hotspots-in -->
+		
+        
+
+
+						
+						
+		<div id="zoom-parent">
+			 <img class="animation-image" id="default_image" src="img/zoom_animation/multi_touch_zoom_00000.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00001.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00002.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00003.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00004.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00005.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00006.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00007.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00008.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00009.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00010.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00011.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00012.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00013.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00014.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00015.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00016.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00017.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00018.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00019.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00020.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00021.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00022.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00023.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00024.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00025.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00026.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00027.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00028.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00029.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00030.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00031.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00032.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00033.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00034.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00035.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00036.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00037.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00038.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00039.jpg">
+			 <img class="animation-image" src="img/zoom_animation/multi_touch_zoom_00040.jpg">
+		</div> 
+
+
+         <div id="right-nav-wrap">
+            <div id="logo-wrap"></div>
+            <nav class="nav-right">
+                <ul>
+                    <li class="load-page" data-page="the-views" data-height="90"><span>check out</span>the views</li>
+                    <li class="load-page" data-page="amenities" data-height="200"><span>view the</span>amenities</li>
+                    <li class="load-page" data-page="the-suites" data-height="310"><span>see inside</span>the suites</li>
+                    <li class="load-page" data-page="community" data-height="420"><span>learn about</span>community</li>
+                    <li class="load-page" data-page="quick-facts" data-height="525"><span>view our</span>quick facts</li>
+                </ul>
+            </nav>
+            <div id="menu-toggle-right" class="menu-toggle">asdfadsf</div>
+        </div>
+        
+        <div id="zoom-controls-wrapper">
+            <div id="zoom-controls">
+                <div id="zoom_out" class="on">-</div>
+                ZOOM
+                <div id="zoom_in">+</div>
+            </div>
+        </div>
+
+		</div> <!-- /viewport-container  -->
+	
+        
+
+        <script src="js/vendor/jquery-2.1.1.min.js"></script>
+        <script src="js/vendor/jquery.panorama.js"></script>
+				<script src="js/animation.js"></script>
+        <script src="js/main.js"></script>
+				
+        <script>
+            $(function(){
+                $("img.advancedpanorama").panorama({
+                     auto_start: 0,
+                    start_position: 1527
+                    });
+
+            });
+        </script>
+    </body>
 </html>
