c6e56379ec10f50555c686ec80fe2c6a6a2d9496
Peter Laird <Peter@Peters-iMac.local>
2014-12-22 16:31:16 -0800

    my first git commit

--- includes/pages/amenities.php
+++ includes/pages/amenities.php
@@ -30,39 +30,56 @@
     	<a class="close-menu-content close-left-menu-button"></a>
 		<div class="carousel-content amenities-carousel-content">
 			<div>
-				<h1>POOL &amp; SPA</h1>
-				<h2>sub title</h2>
-				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to m
+				<h1>INFINITY POOL</h1>
+				<h2>&amp; whirlpool</h2>
+				<p>Unwind in style and comfort in the exquisite indoor infinity pool and whirlpool while admiring picture-perfect views of the North Shore mountains and Fraser River. Swim in the oxygenated lap pool located inside a temperature-controlled environment. Private lockers, showers and change rooms add convenience.
 				</p>
 			</div>
 			<div>
-				<h1>FITNESS CENTER</h1>
-				<h2>sub title</h2>
-				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to m
+				<h1>STEAM ROOM</h1>
+				<h2>&amp; sauna</h2>
+				<p>Enjoy therapeutic benefits in the large sauna and steam room conveniently located next to the pool area. Relax in the wood-paneled sauna equipped with a glass entry door for visibility and security. Rejuvenate in the steam room, which is adorned in mosaic glass tiles and has multi-level benches with ample seating. Truly a luxurious retreat after a long and hectic day. 
 				</p>
 			</div>
 			<div>
-				<h1>THEATER</h1>
-				<h2>sub title</h2>
-				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to m
+				<h1>FITNESS</h1>
+				<h2>centre</h2>
+				<p>Exercise in the spacious gym facility fitted with state-of-the-art machines and equipment.  Or meditate and relax in the adjacent yoga area, specially designed with mood lighting and a Zen-like atmosphere for ultimate personal rejuvenation. Either way, an efficient ventilation system ensures clean air circulation and stimulates restoration of body and mind.
 				</p>
 			</div>	
 			<div>
-				<h1>INDOOR GOLF SIMULATOR</h1>
-				<h2>sub title</h2>
-				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to m
+				<h1>PRIVATE</h1>
+				<h2>theatre</h2>
+				<p>Enjoy the latest Hollywood blockbusters in an immersive, authentic movie theatre.  Delight in the best, private movie-going experience with a top-tier projector and high-tech surround sound audio system.  Specifically designed to offer residents a magical audio and visual adventure without ever leaving home.
 				</p>
 			</div>	
 			<div>
-				<h1>BILLIARDS ROOM</h1>
-				<h2>sub title</h2>
-				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to m
+				<h1>CLUB</h1>
+				<h2>room</h2>
+				<p>Celebrate in style in the exclusive entertainment area, fully equipped for all festive occasions from private dinner parties to larger celebrations. Guaranteed to enchant guests with the most up-to-date audio and visual system and fully equipped kitchen, wet bar and refrigerator.  A spacious outdoor deck with additional seating and built-in barbecue make it the quintessential venue for entertaining.
 				</p>
 			</div>	
 			<div>
-				<h1>PIANO ROOM</h1>
-				<h2>sub title</h2>
-				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to m
+				<h1>BILLIARDS</h1>
+				<h2>room</h2>
+				<p>End a perfect evening of celebration with a friendly game of billiards just steps from the Club Room.  Play in style in the posh, contemporary billiards room featuring a top-of-the-line pool table, wet bar, and casual seating.  A superb venue for catching up with family and friends.
+				</p>
+			</div>	
+			<div>
+				<h1>GAMES</h1>
+				<h2>room</h2>
+				<p>Challenge guests and neighbours to a game of ping-pong. The spacious and well-lit table tennis room is equipped with a top quality, tournament-standard table for leisure and competitive play.  The game and the room is sure to delight all ages and players from tots to pros.  
+				</p>
+			</div>	
+			<div>
+				<h1>MUSIC</h1>
+				<h2>room</h2>
+				<p>Play with wild abandon. The soundproofed music room offers residents a wonderful place to practice quietly or as loudly as desired without disrupting others.  Equipped with an upright piano, music stand and seating, it is an ideal place for both budding and professional musicians to practice.
+			</div>	
+			<div>
+				<h1>STUDY</h1>
+				<h2>room</h2>
+				<p>Study, read, research or write in peace and tranquility in the private library-like setting.  The Study Room is specially designed for academic and scholarly exploits with ample seating and table space for both group studies and private tutorials.
 				</p>
 			</div>				
 		</div>
